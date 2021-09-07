24dfd58f91d8bbdf8581287b8d5102e351c1ade6
Ankushh7 <ankushchaudh456@gmail.com>
2021-09-07 12:16:40 +0530

    Create LogUser.java

---
+++ backend/src/main/java/com/fleet/backend/entity/LogUser.java
@@ -0,0 +1,30 @@
+package com.fleet.backend.entity;
+
+public class LogUser {
+
+	private String usermail;
+	private String password;
+
+	public LogUser(String usermail, String password) {
+		super();
+		this.usermail = usermail;
+		this.password = password;
+	}
+
+	public String getUsermail() {
+		return usermail;
+	}
+
+	public void setUsermail(String usermail) {
+		this.usermail = usermail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
