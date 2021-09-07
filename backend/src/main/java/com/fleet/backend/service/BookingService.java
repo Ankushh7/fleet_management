30928a640c12c160b933b6fd4a1527863d1b7856
Ankushh7 <ankushchaudh456@gmail.com>
2021-09-07 12:17:05 +0530

    Create BookingService.java

---
+++ backend/src/main/java/com/fleet/backend/service/BookingService.java
@@ -0,0 +1,71 @@
+package com.fleet.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fleet.backend.entity.Booking;
+import com.fleet.backend.entity.Car;
+import com.fleet.backend.repository.BookingRepository;
+
+
+@Service
+public class BookingService {
+
+	private BookingRepository bookRepository;
+
+	@Autowired
+	public BookingService(BookingRepository bookRepository) {
+
+		this.bookRepository = bookRepository;
+	}
+
+	public BookingRepository getBookRepository() {
+		return bookRepository;
+	}
+
+	public void setBookRepository(BookingRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	public Booking getByBookMail(String usermail) {
+		return bookRepository.getByBookMail(usermail);
+	}
+
+	public void saveMyBook(Booking book) {
+		bookRepository.save(book);
+	}
+
+	public List<Car> showCars(int hubbid) {
+		return bookRepository.findAllCars(hubbid);
+	}
+
+	public Booking getBookingByBId(int bookid) {
+		return bookRepository.getBookingByBId(bookid);
+	}
+	
+	public void deleteByBookId(int bookid) {
+		this.bookRepository.deleteByBookId(bookid);
+	}
+
+
+	public void saveMyBookFinal(int carid) {
+		Car car = bookRepository.findById(carid);
+
+	}
+
+	public Car findById(int carid) {
+		return this.bookRepository.findById(carid);
+	}
+	
+     public List<Booking> findAllBookings()
+     {
+    	 return this.bookRepository.findAllBookings();
+     }
+     
+     public List<Booking> viewMyBooking(int userid)
+     {
+    	return this.bookRepository.viewMyBooking(userid);
+     }
+}
